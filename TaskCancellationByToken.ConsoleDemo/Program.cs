e416d5d454d3d6a813d938c6cccb5f624986b7be
Raj Kumar1 <Raj.Kumar1@rsystems.com>
2019-10-11 14:14:26 +0530

    Initial commit.

---
+++ TaskCancellationByToken.ConsoleDemo/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskCancellationByToken.ConsoleDemo
+{
+    class Program
+    {
+
+        public static Task PrintWithCancellationToken(int count, CancellationToken cancellationToken)
+        {
+            for (int i = 1; i <= count; i++)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    break;
+                }
+                else {
+                    Console.Write(nameof(PrintWithCancellationToken));
+                    Console.WriteLine(i);
+                }
+                Thread.Sleep(10);
+            }
+
+            return Task.FromResult(0);
+        }
+
+
+        public static Task Print(int count)
+        {
+
+            for (int i = 1; i <= count; i++)
+            {
+                Console.Write(nameof(Print));
+                Console.WriteLine(i);
+                Thread.Sleep(10);
+            }
+            return Task.FromResult(0);
+
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(async () =>
+                await Print(500),
+                async () =>
+                {
+                    using (CancellationTokenSource cTokenSource = new CancellationTokenSource())
+                    {
+                        var keyBoardTask = Task.Run(() =>
+                        {
+                            Console.WriteLine("Press enter to cancel");
+                            Console.ReadKey();
+
+                            // Cancel the task
+                            cTokenSource.Cancel();
+                        });
+
+                        await PrintWithCancellationToken(500, cTokenSource.Token);
+                    }
+                }
+                );
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TaskCancellationByToken.WebApiDemo/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskCancellationByToken.WebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("WithCancellationToken")]
+        public ActionResult<IEnumerable<string>> Get(CancellationToken cancellationToken)
+        {
+            List<string> lst = new List<string>();
+            for (int i = 1; i <= 10; i++)
+            {
+                //cancellationToken.ThrowIfCancellationRequested(); //System.OperationCanceledException: 'The operation was canceled.'
+
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    Debug.WriteLine("Task Cancled");
+                    break;
+                }
+                else
+                {
+                    var value = "value" + i;
+                    lst.Add(value);
+                    Debug.WriteLine(value);
+                }
+                Thread.Sleep(1000);
+            }
+            Debug.WriteLine("Task Finished");
+            return lst;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            List<string> lst = new List<string>();
+            for (int i = 1; i <= 10; i++)
+            {
+                var value = "value" + i;
+                lst.Add(value);
+                Debug.WriteLine(value);
+                Thread.Sleep(1000);
+            }
+            Debug.WriteLine("Task Finished");
+            return lst;
+        }
+
+    }
+}
